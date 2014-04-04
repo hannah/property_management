791bbafb2997599a074f1510cfbba77481f1f82e
Hannah Zabarsky <h.zabarsky@gmail.com>
2014-04-04 19:42:10 -0400

    all of my tests pass now? i don't know anymore

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -3,4 +3,5 @@ class Owner < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, format: {with: regex }
+  has_many :buildings
 end
