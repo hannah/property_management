b1b7862b807dfbd9ba3fd75869ac5b210465f280
Eric Kelly <heroiceric@gmail.com>
2014-04-07 15:17:46 -0400

    Use a constant for EMAIL_REGEX

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,7 +1,8 @@
 class Owner < ActiveRecord::Base
-  regex = /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\Z/
+  EMAIL_REGEX = /\A(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})\Z/
+
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, format: {with: regex }
+  validates :email, presence: true, format: {with: EMAIL_REGEX }
   has_many :buildings
 end
